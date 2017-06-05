2acd8d7fb14619a5da642b65941266ee5fab6894
Claude Seguret <claude.seguret@laposte.fr>
2017-06-05 19:28:42 +0200

    Contextualisation Formulaire

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function hoverPresentation(element) {
+    element.setAttribute('src', '/img/presentation_service_hover.png');
+}
+function unhoverPresentation(element) {
+    element.setAttribute('src', '/img/presentation_service.png');
+}
+
