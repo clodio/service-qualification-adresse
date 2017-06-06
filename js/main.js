ee9b6c2355b388b867d985c0a43737f73d69b661
Claude Seguret <claude.seguret@laposte.fr>
2017-06-06 18:28:48 +0200

    Gestion CORS et erreur Jquery sur 202 sans body

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function hoverPresentation(element) {
-    element.setAttribute('src', '/img/presentation_service_hover.png');
+    element.setAttribute('src', 'img/presentation_service_hover.png');
 }
 function unhoverPresentation(element) {
-    element.setAttribute('src', '/img/presentation_service.png');
+    element.setAttribute('src', 'img/presentation_service.png');
 }
 
 
@@ -10,59 +10,82 @@ $(document).ready(function(){
     $("#formFileSubmit").click(function(e){
         e.preventDefault();
     
-    // TODO : Add control (email mandatory, file-type .csv)
-    var file = $("#file").serialize();
-    var data = new FormData();
-    jQuery.each(jQuery('#file')[0].files, function(i, file) {
-        data.append('data', file);
-    });
+        // TODO : Add control (email mandatory, file-type .csv)
+        var file = $("#file").serialize();
 
-    $("#file_transfer").show();
-    $("#formFile").hide();
+        // Récupère le fichier pour l'envoyer à l'API
+        var data = new FormData();
+        jQuery.each(jQuery('#file')[0].files, function(i, file) {
+            data.append('data', file);
+        });
 
-    var query = "?params=" + $("#params").val().toString() + 
-    "&data_format=" + $('input[name=data_format]:checked', '#formFile').val().toString() + "&notification_email=" + $("#notification_email").val() + "&csv_numeric_format=" + $("#csv_numeric_format").val() + "&my_fields=" + $("#my_fields").val();
-    var request = $.ajax({
-        async: true,
-        crossDomain: true,
-        accept:"application/json",
-        headers: {},
-        processData: false,
-        mimeType: "multipart/form-data",
-        url: "/address/v1/batch_analysis" + query,
-        method: "POST",
-        contentType : "multipart/form-data",
-        data: data,
-        dataType: "json"
-    }, function(data, statusText, jqXHR) {
-        alert( "success" );
+        // Affiche le panneau de transfert de fichier
         $("#file_transfer").show();
         $("#formFile").hide();
-        $("#file_error").hide();
-        $("#file_success").hide();
-    });
-    
-    request.done(function(data, statusText, jqXHR) {
-        alert( "second success" );
-        $("#file_transfer").hide();
-        $("#formFile").hide();
-        $("#file_error").hide();
-        $("#file_success").show();
-        $("#batch-uid").val("todo");
-    });
-    
-    request.fail(function(jqXHR, textStatus) {
-        console.log(jqXHR);
-        $("#file_transfer").hide();
-        $("#formFile").hide();
-        $("#file_error").show();
-        $("#file_success").hide();
-    });
 
-    request.always(function() {
-        // alert( "finished" );
-    });
+        var query = "?params=" + $("#params").val().toString() + "&notification_email=" + $("#notification_email").val() + "&csv_numeric_format=" + $("#csv_numeric_format").val() + "&my_fields=" + $("#my_fields").val();
 
+        // Ajout du parametre data_format si non vide
+        if ( $('input[name=data_format]:checked', '#formFile').val().toString() !="" ){
+            query= query + "&data_format=" + $('input[name=data_format]:checked', '#formFile').val().toString();
+        }
+
+        var request = $.ajax({
+            async: true,
+            crossDomain: true,
+            accept:"application/json",
+            headers: {},
+            processData: false,
+            mimeType: "multipart/form-data",
+            url: "http://ws-apiqa-sdev.apps.paas.dev.net-courrier.extra.laposte.fr" +  "/address/v1/batch_analysis" + query,
+            method: "POST",
+            contentType: false,
+            data: data,
+            dataType: "json"
+        }, function(data, statusText, jqXHR) {
+            // Affichage du panneau de transfert de fichier
+            $("#file_transfer").show();
+            $("#formFile").hide();
+            $("#file_error").hide();
+            $("#file_success").hide();
+        });
+        
+        request.done(function(data, statusText, jqXHR) {
+            if ( jqXHR.status == "202") {
+                // Affichage du panneau de succès
+                $("#batch-uid").val(jqXHR.getResponseHeader('Location').split('/').pop());
+                $("#file_error").hide();
+                $("#file_success").show();
+            }
+            else {
+                // Ce cas ne devrait pas arriver
+                // Affichage d'une erreur
+                $("#file_error").show();
+                $("#file_success").hide();
+                $("#file_error_message").text("Erreur Serveur " + jqXHR.status);
+            }
+            $("#file_transfer").hide();
+            $("#formFile").hide();
+        });
+        
+        request.fail(function(jqXHR, textStatus, error) {
+            if ( jqXHR.status == "202") {
+                // Code présent du fait d'un bug jquery 
+                // Dans le cas de code 202 avec body vide jquery considère que c'est une erreur
+                $("#batch-uid").val(jqXHR.getResponseHeader('Location').split('/').pop());
+                $("#file_error").hide();
+                $("#file_success").show();
+            }
+            else {
+                // Affichage d'une erreur
+                // TODO : afficher un numéro d'erreur plus pertinent
+                $("#file_error").show();
+                $("#file_success").hide();
+                $("#file_error_message").text("Erreur Serveur " + jqXHR.status);
+            }
+            $("#file_transfer").hide();
+            $("#formFile").hide();
+        });
     });
 
     $("#file_success_new").click(function(e){
@@ -80,7 +103,4 @@ $(document).ready(function(){
             $("#file_error").hide();
             $("#file_success").hide();    
     });
-
-
 });
-
